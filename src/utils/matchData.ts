bb9c8df9b103b7d8a2e11a67cdc1319b73bed27f
Sol Lanosa <mslanosa@hotmail.com>
2024-02-20 00:11:49 -0300

    fix: match function to avoid duplicates

--- src/utils/matchData.ts
+++ src/utils/matchData.ts
@@ -2,24 +2,44 @@ import { Startup, RawInvestor, Investor } from "src/types";
 
 export const matchData = (investors: RawInvestor[], startups: Startup[]) => {
   const newMatches: { [key: string]: Investor } = {};
+  let matchedStartups: Startup[] = [];
 
-  investors.forEach((investor) => {
+  const investorsSorted = investors.sort((a, b) => {
+    const nameA = a.industry.toUpperCase();
+    const nameB = b.industry.toUpperCase();
+    return nameB.localeCompare(nameA);
+  });
+
+  investorsSorted.forEach((investor) => {
     const investorName = investor.name;
     const industryPreference = investor.industry;
-
-    let matchedStartups: Startup[] = [];
+    let startupSliced: Startup[] = [];
     if (industryPreference === "any") {
-      matchedStartups = startups.slice(0, 10);
+      const startupName = new Set(
+        matchedStartups.map(({ name }: Startup) => name)
+      );
+      const filteredStartup = startups.filter(
+        ({ name }: Startup) => !startupName.has(name)
+      );
+      startupSliced = filteredStartup.slice(0, 10);
+      matchedStartups = matchedStartups.concat(startupSliced);
     } else {
-      matchedStartups = startups
+      const startupName = new Set(
+        matchedStartups.map(({ name }: Startup) => name)
+      );
+      const filteredStartup = startups.filter(
+        ({ name }: Startup) => !startupName.has(name)
+      );
+      startupSliced = filteredStartup
         .filter((startup) => startup.industry === industryPreference)
         .slice(0, 10);
+      matchedStartups = matchedStartups.concat(startupSliced);
     }
 
     newMatches[investorName] = {
       name: investorName,
       industry: industryPreference,
-      startups: matchedStartups,
+      startups: startupSliced,
       deletedStartups: [],
       originalName: investorName,
     };
